ceabe475f526ec834da43a23d734eacd4cb5de4b
Erik Diniz <116113584+erikdiniz@users.noreply.github.com>
2023-08-16 09:24:22 -0300

    Add files via upload

---
+++ R01-02-Rot-SimpleSorting-Bidirectional-Bubble-environment/src/main/java/sorting/simpleSorting/InsertionSort.java
@@ -0,0 +1,26 @@
+package sorting.simpleSorting;
+
+import sorting.AbstractSorting;
+import util.Util;
+
+/**
+ * As the insertion sort algorithm iterates over the array, it makes the
+ * assumption that the visited positions are already sorted in ascending order,
+ * which means it only needs to find the right position for the current element
+ * and insert it there.
+ */
+public class InsertionSort<T extends Comparable<T>> extends AbstractSorting<T> {
+
+	@Override
+	public void sort(T[] array, int leftIndex, int rightIndex) {
+		for (int i = rightIndex; i > leftIndex; i--){
+			int pos = i;
+			for (int j = i-1; j > leftIndex; j--){
+				if (array[pos].compareTo(array[j]) < 0){
+					pos = j;
+				}
+			}
+			Util.swap(array, i, pos);
+		}
+	}
+}
